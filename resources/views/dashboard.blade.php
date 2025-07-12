abe14e2587109926d2ad4a1c1bc4e37e5901ea9f
mrwilde <robert@mrwilde.com>
2025-07-12 23:00:27 +1000

    refactor: replace inline Livewire component call with Blade directive in `dashboard.blade.php`

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,5 +1,5 @@
 <x-layouts.app :title="__('Dashboard')">
     <div class="flex h-full w-full flex-1 flex-col gap-6">
-        <livewire:calendar-view-simple />
+        @livewire('calendar-view-simple')
     </div>
 </x-layouts.app>
